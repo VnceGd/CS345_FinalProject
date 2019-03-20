c833e2a3c71ee4962b66397cbd98d80677379a48
Tom T <spidy212@msn.com>
2019-03-20 19:07:11 -0400

    Release of Near Complete Build

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] Sounds;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach (Sound s in Sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play (string name)
+    {
+       Sound s = Array.Find(Sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
